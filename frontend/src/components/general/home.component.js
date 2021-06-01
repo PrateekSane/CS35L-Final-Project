d838456a1bfae56267d7b4bd9d197e695ed70223
Nitin Subramanian <nitin@zooglr.com>
2021-06-01 15:30:21 -0700

    home component changes

--- frontend/src/components/general/home.component.js
+++ frontend/src/components/general/home.component.js
@@ -1,27 +1,39 @@
 import React from "react";
+import './home.css';
+import Tweet from "./tweet.component";
+import axios from 'axios';
 
-const Home = () => {
-  return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        paddingTop: "50px",
-      }}
-    >
-      <div>HOME PAGE THAT NEEDS TO BE REVISED</div>
-      <p>random links so its easy to access:</p>
-      <a href="/login" style={{ fontSize: "50px" }}>
-        Login
-      </a>
-      <a href="/signup" style={{ fontSize: "50px" }}>
-        Signup
-      </a>
-      <a href="/createPost" style={{ fontSize: "50px" }}>
-        Create Post
-      </a>
-    </div>
-  );
-};
+class Home extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      data: []
+    }
+  }
+  componentDidMount() {
+    axios.get('http://localhost:5000/getAllTweets')
+      .then(res => {
+        const data = res.data;
+        this.setState({ data: res.data });
+      })
+      this.state.data.sort();
+  }
+
+render() {
+    return (
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          paddingTop: "50px",
+        }}
+      >
+      {[].concat(this.state.data).sort((a, b) => a.likes < b.likes ? 1 : -1).map((d) => (
+        <Tweet cur={{title: d.title, body: d.body, likes: d.likes, shares: d.shares, tag: d.tags}}/>
+      ))}
+      </div>
+    );
+  };
+}
 export default Home;
